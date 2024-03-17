4cb6df9d45dd1c12522f45600525e9fb8c5f0020
Lexach <79162875844@mail.ru>
2024-03-17 18:18:10 +0300

    add new library

---
+++ src/PhoneValidatorRU.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace src;
+class PhoneValidatorRU
+{
+    const PHONE_LENGTH = 11;
+
+    function validate(string $phoneNumber): bool
+    {
+        $phoneNumber = preg_replace('/\D/', '', $phoneNumber);
+
+        if (preg_match('/^\+?\d{1,3}?\d{9,12}$/', $phoneNumber)
+            && strlen($phoneNumber) === self::PHONE_LENGTH
+            && preg_match('/^(?:\+7|8)\d{10}$/', $phoneNumber)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
